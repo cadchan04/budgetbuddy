a720946a4da008189c283a3d34ca29272a383eb9
cadchan04 <chan377@purdue.edu>
2024-01-20 15:58:05 -0500

    added new budget form

--- budgetbuddy/src/pages/Dashboard.jsx
+++ budgetbuddy/src/pages/Dashboard.jsx
@@ -1,24 +1,42 @@
 // rrd imports
 import { useLoaderData } from "react-router-dom";
 
-// helper functions
+// components
+import AddBudgetForm from "../components/AddBudgetForm";
 
+// helper functions
 import {fetchData} from "../helpers"
 
 // loader
 export function dashboardLoader() {
     const userName = fetchData("userName");
-    return { userName }
+    const budgets = fetchData("budgets")
+    return { userName, budgets }
 }
 
 const Dashboard = () => {
-    const { userName } = useLoaderData()
+    const { userName, budgets } = useLoaderData()
 
     return (
-        <div>
-            <h1>{userName}</h1>
-            Dashboard
-        </div>
+        // <div>
+        //     <h1>{userName}</h1>
+        //     Dashboard
+        // </div>
+        <>
+            {userName ? (
+            <div className="dashboard">
+                <h1>Welcome back, <span className="accent">{userName}</span></h1>
+                <div className="grid-sm">
+                    {/* {budgets ? () : ()} */}
+                    <div className="grid-lg">
+                        <div className="flex-lg">
+                            <AddBudgetForm />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            ) : <Intro />}
+        </>
     )
 }
 export default Dashboard
